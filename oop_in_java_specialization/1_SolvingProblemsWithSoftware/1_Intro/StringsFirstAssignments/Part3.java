3ce0a58a2602d4e300fb8ffea723e4c14b10a165
Chris Chung <chchung@rf.sitesafe.com>
2023-07-21 17:23:06 -0400

    Complete part 3 and 4 of assignment.

--- oop_in_java_specialization/1_SolvingProblemsWithSoftware/1_Intro/StringsFirstAssignments/Part3.java
+++ oop_in_java_specialization/1_SolvingProblemsWithSoftware/1_Intro/StringsFirstAssignments/Part3.java
@@ -30,11 +30,26 @@ public class Part3 {
         System.out.println("a in banana"+ twoOccurrences("a", "banana ") + "\n"); // true
         System.out.println("an in banana"+ twoOccurrences("an", "banana ") + "\n"); // true
         System.out.println("c in banana"+ twoOccurrences("c", "banana ") + "\n"); // false
+        
+        System.out.println(("bro in broccoli is = ") + lastPart("bro", "broccoli"));
+        System.out.println(("car in truck is = ") + lastPart("car", "truck"));
     }
     
     //
     public String lastPart (String stringa, String stringb) {
-        //
+        // Find 1st occurs of stringa in stringb. Then return what follows it.
+        // If no occurence of stringa, then return stringb.
+        int aIndex = stringb.indexOf(stringa);
+        int aLength = stringa.length();
+        
+        if (aIndex == -1) {
+            System.out.println("no occurence");
+            return stringb;
+        } else {
+            System.out.println("occurence");
+            return stringb.substring(aIndex + aLength);
+        }
+        
     }
 }
 
