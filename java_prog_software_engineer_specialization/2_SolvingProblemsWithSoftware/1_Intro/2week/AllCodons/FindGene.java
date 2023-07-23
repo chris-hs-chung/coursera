b6dc889445224cc8e34a8b2f0f492bb609648d7c
Chris Chung <chchung@rf.sitesafe.com>
2023-07-22 21:06:24 -0400

    week 2: FindGene.java is the simple version of finding a gene.

---
+++ java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/1_Intro/2week/AllCodons/FindGene.java
@@ -0,0 +1,55 @@
+
+/**
+ * A simple version of finding a gene in a dna sequence.
+ * 
+ * @author chris
+ * @version 7/22/2023
+ */
+import edu.duke.*;
+
+public class FindGene {
+    public String findGene (String dna) {
+        // Find a valid gene that starts with ATG and ends with TAA.
+        // The length must be a multiple of three.
+
+        int startIndex = dna.indexOf("ATG");
+        if (startIndex == -1) {
+            return "";
+        }
+
+        int currIndex = dna.indexOf("TAA", startIndex + 3);
+
+        while (currIndex != -1) {
+            if ((currIndex - startIndex) % 3 == 0) {
+                return dna.substring(startIndex, currIndex + 3);
+            } else {
+                currIndex = dna.indexOf("TAA", currIndex + 1);
+            }
+        }
+        
+        return "";
+    }
+
+    public void testGene() {
+        String dna;
+        dna = "ATGTAA";
+        System.out.println("DNA= " + dna);
+        System.out.println("Gene= " + findGene(dna) + "\n");
+
+        dna = "ATGZTAA";
+        System.out.println("DNA= " + dna);
+        System.out.println("Gene= " + findGene(dna) + "\n");
+
+        dna = "ATGTATA";
+        System.out.println("DNA= " + dna);
+        System.out.println("Gene= " + findGene(dna) + "\n");
+
+        dna = "ATGZZTAA";
+        System.out.println("DNA= " + dna);
+        System.out.println("Gene= " + findGene(dna) + "\n");
+
+        dna = "ZATGAAAGGGTTTTAA";
+        System.out.println("DNA= " + dna);
+        System.out.println("Gene= " + findGene(dna) + "\n");
+    }
+}
