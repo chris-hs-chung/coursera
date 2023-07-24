e958a415312b288c920746042ff9f15bed8cbe82
Chris Chung <chchung@rf.sitesafe.com>
2023-07-24 12:59:26 -0400

    Completion of part 1 of strings 2nd assignment.

--- java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/1_Intro/2_week/StringsSecondAssignment/Part1.java
+++ java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/1_Intro/2_week/StringsSecondAssignment/Part1.java
@@ -16,16 +16,76 @@ public class Part1 {
         int currIndex = dna.indexOf(stopCodon, startIndex);
 
         while (currIndex != -1) {
-            if ({currIndex - startIndex} % 3 == 0) {
+            if ((currIndex - startIndex) % 3 == 0) {
                 return currIndex;
             } else {
-                currIndex = dna.indexOf(stopCodon, currIndex + 3)
+                currIndex = dna.indexOf(stopCodon, currIndex + 3);
             }
         }
 
-        if (currIndex == -1) {
-            return dna.length();
+         return dna.length();
+    }
+    
+    // find a gene
+    public String findGene (String dna) {
+        // Find first occurence of ATG.
+        int startIndex = dna.indexOf("ATG");
+        if (startIndex == -1) {
+            return "";
         }
+        
+        // stop codons = TAA, TAG, TGA
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        
+        // find lowest valid stop codon index
+        int temp = Math.min(taaIndex, tagIndex);
+        int minIndex = Math.min(temp, tgaIndex);
+        
+        if (minIndex != dna.length()) {
+            return dna.substring(startIndex, minIndex + 3);
+        }
+        
+        return "";
+    }
+    
+    // test findGene
+    public void testFindGene () {
+        String dna = "ATGTAA";
+        int startIndex = dna.indexOf("ATG");
+        String gene = findGene(dna);
+        System.out.println("dna= " + dna);
+        System.out.println("gene= " + gene);
+        System.out.println("* * *");
+        
+        dna = "ATGzzTAA";
+        startIndex = dna.indexOf("ATG");
+        gene = findGene(dna);
+        System.out.println("dna= " + dna);
+        System.out.println("gene= " + gene);
+        System.out.println("* * *");
+        
+        dna = "zzzATGzzzTAAzz";
+        startIndex = dna.indexOf("ATG");
+        gene = findGene(dna);
+        System.out.println("dna= " + dna);
+        System.out.println("gene= " + gene);
+        System.out.println("* * *");
+        
+        dna = "zzzAGzzzTAAzz";
+        startIndex = dna.indexOf("ATG");
+        gene = findGene(dna);
+        System.out.println("dna= " + dna);
+        System.out.println("gene= " + gene);
+        System.out.println("* * *");
+        
+        dna = "zzzATGzzTAAzZzzTGA";
+        startIndex = dna.indexOf("ATG");
+        gene = findGene(dna);
+        System.out.println("dna= " + dna);
+        System.out.println("gene= " + gene);
+        System.out.println("* * *");
 
     }
 }
