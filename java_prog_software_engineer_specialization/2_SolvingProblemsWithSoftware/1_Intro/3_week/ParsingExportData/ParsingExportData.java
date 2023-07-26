ade9b03c4508c81af1a013d98380265645502129
chris-hs-chung <chris.hs.chung@gmail.com>
2023-07-26 19:36:20 -0400

    Completion of ParsingExportData assignment
    Used apache library to read CSV files.

--- java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/1_Intro/3_week/ParsingExportData/ParsingExportData.java
+++ java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/1_Intro/3_week/ParsingExportData/ParsingExportData.java
@@ -8,16 +8,58 @@ import org.apache.commons.csv.*;
  * @version 7/26
  */
 public class ParsingExportData {
+    // Part 5
+    public void bigExporters (CSVParser parser, String amount){
+        // Print countries along with their amounts that are greater than the length of the invoked amount.
+        String country = "";
+        String a = "";
+        int amountLength = amount.length();
+        int aLength = 0;
+        for (CSVRecord record : parser) {
+            country = record.get("Country");
+            a = record.get("Value (dollars)");
+            aLength = a.length();
+            if (aLength > amountLength){
+                System.out.println(country + " " + a);
+            }
+            
+        }        
+    }
     
+    // Part 4
+    public int numberOfExporters (CSVParser parser, String exportItem) {
+        // Return the # of countries that export a certain item.
+        int count = 0;
+        for (CSVRecord record : parser) {
+            String item = record.get("Exports");
+            if (item.contains(exportItem)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    // Part 3
+    public void listExportersTwoProducts (CSVParser parser, String exportItem1 , String exportItem2){
+        // Prints the countries with certain exports.
+        
+        for (CSVRecord record : parser) {
+            String item = record.get("Exports");
+            if ( item.contains(exportItem1) && item.contains(exportItem2)) {
+                System.out.println(record.get("Country"));
+            }
+        }
+    }
+    
+    // Part 2
     public String countryInfo (CSVParser parser, String country) {
         // Return info about a country.
         String info = "";
         
         for (CSVRecord record : parser) {
-            //System.out.println(record.get("Country"));
-            String line = record.toString();
-            if (line.contains(country)) {
-                info = info + record.get("Country") + ": ";
+            String getCountry = record.get("Country");
+            if (getCountry.contains(country)) {
+                info = info + getCountry + ": ";
                 info = info + record.get("Exports") + ": ";
                 info = info + record.get("Value (dollars)");
                 return info;
@@ -26,13 +68,25 @@ public class ParsingExportData {
         return "NOT FOUND";
     }
     
-    // Test
+    // Part 1 : Test
     public void tester() {
             FileResource fr = new FileResource();
             CSVParser parser = fr.getCSVParser();
-            String country = "Germany";
+            String country = "Nauru";
             
+            System.out.println("part 2");
             System.out.println(countryInfo(parser, country));
             
+            System.out.println("part 3");
+            parser = fr.getCSVParser();
+            listExportersTwoProducts(parser, "gold", "diamonds");
+            
+            System.out.println("part 4");
+            parser = fr.getCSVParser();
+            System.out.println("Count= " + numberOfExporters(parser, "gold"));
+            
+            System.out.println("part 5");
+            parser = fr.getCSVParser();
+            bigExporters(parser, "$999,999,999,999");
     }
 }
