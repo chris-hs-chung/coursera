d603d38ebdc5f728f2d5d3c17711a0ea914a7d59
Chris Chung <chchung@rf.sitesafe.com>
2023-07-27 18:51:38 -0400

    MaxTemp.class from lecutre:
    parse multiple csv files to find the row with highest temperature.

--- java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/3_week/MaxTemp/MaxTemp.java
+++ java_prog_software_engineer_specialization/2_SolvingProblemsWithSoftware/3_week/MaxTemp/MaxTemp.java
@@ -2,43 +2,52 @@ import edu.duke.*;
 import org.apache.commons.csv.*;
 import java.io.*;
 
-/**
- * A prorgram to find the hottest temp from CSV files
- */ 
- */
 
 public class MaxTemp {
     public CSVRecord hottestHourInDay (CSVParser parser){
         // returns the CSV row with the hottest temp from a single file.
         CSVRecord hottestRow = null;
-        double currentTemp = 0.0;
-        double hottestTemp = 0.0;
         
         for (CSVRecord currentRow : parser){
-            if (hottestRow == null){ 
-                hottestRow = currentRow;
-            }
-            currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-            hottestTemp = Double.parseDouble(hottestRow.get("TemperatureF"));
-            
-            if (currentTemp > hottestTemp){
-                hottestRow = currentRow;
-            }
+            hottestRow = getLargestOfTwo(currentRow,hottestRow);
         }
         return hottestRow;
     }
     
-    public void testHottestHourDay(){
-        // test hottestHourInDay
-        FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
-        CSVRecord hottestRow = hottestHourInDay(parser);
-        System.out.println( "The hottest day= " + hottestRow.get("DateUTC"));
-        System.out.println( "time= " + hottestRow.get("TimeEST"));
-        System.out.println( "temp= " + hottestRow.get("TemperatureF") + " F");
-        System.out.println( "humidity= " + hottestRow.get("Humidity"));
-        System.out.println( "conditions= " + hottestRow.get("Conditions"));
+    public CSVRecord hottestInManyDays(){
+        // return a CSVRecord row with the hottest temp amoung multiple rows
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord hottestRow = null;
+        
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVParser parser = fr.getCSVParser();
+            
+            CSVRecord currentRow = hottestHourInDay(parser);
+            hottestRow = getLargestOfTwo(currentRow, hottestRow);
+
+        }
+        return hottestRow;
     }
     
+    public CSVRecord getLargestOfTwo(CSVRecord currentRow, CSVRecord hottestRow){
+        // Compare 2 rows, and return the highest
+        if (hottestRow == null) {
+            hottestRow = currentRow;
+        }
+        double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+        double hottestTemp = Double.parseDouble(hottestRow.get("TemperatureF"));
+        if (currentTemp > hottestTemp) {
+            hottestRow = currentRow;
+        }
+        return hottestRow;
+    }
     
+    public void testHottestManyDays(){
+        // test hottestInManyDays
+        CSVRecord hottestRow = hottestInManyDays();
+        System.out.println("Hottest day among many files= " + hottestRow.get("DateUTC"));
+        // System.out.println( "time= " + hottestRow.get("TimeEST"));
+        System.out.println( "temp= " + hottestRow.get("TemperatureF") + " F \n");
+    }
 }
